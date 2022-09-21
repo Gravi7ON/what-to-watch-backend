69b805a5cb0dd7d246b198f68313d0961d601496
Dmitry Radionov <radionovdmitry93@mail.ru>
2022-09-21 21:04:00 +0500

    Создает контейнер IoC

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,7 @@
-import { Container } from 'inversify';
 import 'reflect-metadata';
 import Application from './app/application.js';
 import { Component } from './types/component-types.js';
-import LoggerService from './common/logger/logger-service.js';
-import ConfigService from './common/config/config-service.js';
-import { LoggerInterface } from './common/logger/logger.interface.js';
-import { ConfigInterface } from './common/config/config.interface.js';
-
-const applicationContainer = new Container();
-applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
-applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
-applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
+import { applicationContainer } from './inversify.config.js';
 
 const application = applicationContainer.get<Application>(Component.Application);
 application.init();
