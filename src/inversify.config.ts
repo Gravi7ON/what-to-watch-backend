a80127a07e818755aac98e60ef1317ed980a2b97
Dmitry Radionov <radionovdmitry93@mail.ru>
2022-09-27 13:45:15 +0500

    Реализует подключение к базе данных

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -5,10 +5,13 @@ import { LoggerInterface } from './common/logger/logger.interface.js';
 import { ConfigInterface } from './common/config/config.interface.js';
 import { Component } from './types/component-types.js';
 import Application from './app/application.js';
+import DatabaseService from './common/db-client/db-service.js';
+import { DatabaseInterface } from './common/db-client/db.interface.js';
 
 const applicationContainer = new Container();
 applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
 applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
 applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
+applicationContainer.bind<DatabaseInterface>(Component.DatabaseInterface).to(DatabaseService).inSingletonScope();
 
 export { applicationContainer };
